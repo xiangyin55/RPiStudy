f8e65b7a67b99d58c23dcd1c1bbeae534d4cb34d
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2018-04-08 22:58:49 +0800

    Create rpicam_end_box.py

---
+++ script/rpicam_end_box.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+file_dir = "/mnt/dietpi_userdata/rpicam"
+
+ctime = time.localtime(time.time())
+list = os.listdir(file_dir)
+for f in list:
+        path = os.path.join(file_dir,f)
+        print(path)
+        cmd = "bypy -v upload "+path+" /rpicam/"+time.strftime('%Y/%Y%m/%Y%m%d/',ctime) + os.path.basename(path)
+        os.system("echo exec:" + cmd + " >> /var/log/rpicam`date +%Y%m%d`.log 2>&1" )
+        os.system(cmd +" >> /var/log/rpicam`date +%Y%m%d`.log 2>&1")
+        os.remove(path)
