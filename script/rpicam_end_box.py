d06c31911b21d5eda3575483a3f643ad6b30bfe1
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2018-04-08 23:49:07 +0800

    Update rpicam_end_box.py

--- script/rpicam_end_box.py
+++ script/rpicam_end_box.py
@@ -12,6 +12,6 @@ for f in list:
         path = os.path.join(file_dir,f)
         print(path)
         cmd = "bypy -v upload "+path+" /rpicam/"+time.strftime('%Y/%Y%m/%Y%m%d/',ctime) + os.path.basename(path)
-        os.system("echo exec:" + cmd + " >> /var/log/rpicam`date +%Y%m%d`.log 2>&1" )
-        os.system(cmd +" >> /var/log/rpicam`date +%Y%m%d`.log 2>&1")
+        os.system("echo exec:" + cmd + " >> /var/log/lighttpd/rpicam`date +%Y%m%d`.log 2>&1" )
+        os.system(cmd +" >> /var/log/lighttpd/rpicam`date +%Y%m%d`.log 2>&1")
         os.remove(path)
