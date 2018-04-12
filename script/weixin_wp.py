32cb3e0a878fc81f4e1d234b89d1387db63939a6
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2018-04-12 22:51:30 +0800

    Update weixin_wp.py

--- script/weixin_wp.py
+++ script/weixin_wp.py
@@ -8,11 +8,11 @@ from itchat.content import *
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
   stime = time.strftime('%Y/%Y%m/%Y%m%d/', time.localtime())
-  wxdir = "/root/weixin/"+ stime 
-  phdir = "/root/photos/"+stime
-  byfile = "/weixin/"+ time.strftime('%Y/%Y%m/', time.localtime()) + msg.FileName
-  upcmd = "bypy -v upload " + wxdir + msg.FileName + " " + byfile + " >bypy.file 2>&1 &"
-  imgcmd = "export DISPLAY=:0.0 && nohup /usr/bin/feh -g 1280x768 -rZFD 30 -Smtime /root/photos  >feh.file 2>&1 &"
+  wxdir = "/root/weixin/"+ stime #本地微信附近备份目录
+  phdir = "/root/photos/"+stime  #本地相册文件存储目录
+  byfile = "/weixin/"+ time.strftime('%Y/%Y%m/', time.localtime()) + msg.FileName #云端微信文件名称
+  upcmd = "bypy -v upload " + wxdir + msg.FileName + " " + byfile + " >bypy.file 2>&1 &" #bypy命令上传文件
+  imgcmd = "export DISPLAY=:0.0 && nohup /usr/bin/feh -g 1280x768 -rZFD 30 -Smtime /root/photos  >feh.file 2>&1 &" #播放图片文件
   if not os.path.exists(wxdir): os.makedirs(wxdir)
   if not os.path.exists(phdir): os.makedirs(phdir)
   msg.download(wxdir + msg.FileName)
